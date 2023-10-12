96973b825074117f89d3c032ee8305594c692b68
Chad Stewart <chadrhonanstewart@gmail.com>
2023-10-12 01:43:21 -0500

    Completed created api-gateway container. Made redis container to use redis locally instead of from redis cloud.

--- api-gateway-example/services/redis.ts
+++ api-gateway-example/services/redis.ts
@@ -4,4 +4,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 //Initialize Redis
-export const redis = new Redis(`redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_PUBLIC_URL}`);
+export const redis = new Redis(`redis://${process.env.REDIS_PUBLIC_URL}`);
