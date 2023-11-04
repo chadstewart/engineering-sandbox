3cd060ef9c2581fa04fbd3d6f41a7e92062152d7
Chad Stewart <chadrhonanstewart@gmail.com>
2023-11-03 20:28:21 -0500

    Removed rest api route and all related controllers as all routes outside of auth will be handled by graphql.

--- api-gateway-example/app.ts
+++ api-gateway-example/app.ts
@@ -20,11 +20,9 @@ app.use(rateLimit);
 app.use(checkCache);
 
 //Initialize Routers
-import v1Router from "./v1/routes/gateway-router";
 import v1AuthRouter from "./v1/routes/authn-router";
 
 //Use Routers
-app.use("/v1/gateway", v1Router);
 app.use("/v1/auth", v1AuthRouter);
 app.get("/", (req, res) => res.send("Hello World!!"));
 
--- api-gateway-example/v1/routes/gateway-router.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import getCatImage from "../controllers/cat-controller";
-
-router.get("/cat", getCatImage);
-
-export default router;
