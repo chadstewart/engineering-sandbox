65047f5cde829e66f19a761dc6b5a9ef4d04f2e3
Chad Stewart <chadrhonanstewart@gmail.com>
2023-11-08 00:57:00 -0500

    Updated api function to get it working. Forgot to add the protocol to the url. Also removed the config as it's not needed.

--- api-gateway-example/lib/api/internal-apis/orders/index.ts
+++ api-gateway-example/lib/api/internal-apis/orders/index.ts
@@ -10,33 +10,53 @@ const CONFIG: RequestInit = {
 export const getOrders = async (page = 1) => {
   const data = await api.get(
     zod.object({      
-      order_id: zod.number(),
-      order_date: zod.date().nullable(),
-      shipped_date: zod.date().nullable(),
-      ship_via: zod.number().nullable(),
+      status: zod.string(),
+      data: zod.object({
+        queryData: zod.object({
+          order_id: zod.number(),
+          customer_id: zod.string(),
+          employee_id: zod.number(),
+          order_date: zod.string().nullable(),
+          required_date: zod.string().nullable(),
+          shipped_date: zod.string().nullable(),
+          ship_via: zod.number().nullable(),
+          freight: zod.number().nullable(),
+          ship_name: zod.string().nullable(),
+          ship_address: zod.string().nullable(),
+          ship_city: zod.string().nullable(),
+          ship_region: zod.string().nullable(),
+          ship_postal_code: zod.string().nullable(),
+          ship_country: zod.string().nullable()
+        }).array(),
+        totalPages: zod.number()
+      })
     }),
-    `rest-api:3000/v1/orders/${page}`,
-    CONFIG
+    `http://rest-api:3001/v1/orders/${page}`
   );
-  return data;
+  return data.data.queryData;
 };
 
 export const getOrderDtails = async (orderId = '') => {
   const data = await api.get(
-    zod.object({
-      orders: zod.object({
-          order_date: zod.date().nullable(),
-          shipped_date: zod.date().nullable()
-      }),
-      order_id: zod.number(),
-      unit_price: zod.number(),
-      quantity: zod.number(),
-      products: zod.object({
-          product_id: zod.number()
+    zod.object({      
+      status: zod.string(),
+      data: zod.object({
+        queryData: zod.object({
+          orders: zod.object({
+              order_date: zod.date().nullable(),
+              shipped_date: zod.date().nullable()
+          }),
+          order_id: zod.number(),
+          unit_price: zod.number(),
+          quantity: zod.number(),
+          products: zod.object({
+              product_id: zod.number()
+          })
+        }).array(),
+        totalPages: zod.number()
       })
     }),
-    `rest-api:3000/v1/orders/details/${orderId}`,
-    CONFIG
+    `http://rest-api:3001/v1/orders/details/${orderId}`
   );
   return data;
 };
