f5634b9b959439e5e4fe69df41736ee93b640a13
Chad Stewart <chadrhonanstewart@gmail.com>
2023-11-09 10:23:43 -0500

    Updated graphql resolvers to remove async/await as they seem to not be needed. Seems like the resolvers can handle promises directly.

--- api-gateway-example/graphql/resolvers/index.ts
+++ api-gateway-example/graphql/resolvers/index.ts
@@ -18,37 +18,37 @@ interface CreateEmployeeMutationArgs {
 
 export const resolvers = {
   Query: {
-    getOrders: async (_: any, args: QueryPaginationArgs, context: ResolverContext) => await getOrders(args.page),
-    getOrderDetails: async (_: any, args: { id: string }, context: ResolverContext) => await getOrderDtails(args.id),
-    getEmployees: async (_: any, args: QueryPaginationArgs, context: ResolverContext) => null,
-    getCustomers: async (_: any, args: QueryPaginationArgs, context: ResolverContext) => null,
-    getCustomerDetails: async (_: any, args: { id: string }, context: ResolverContext) => null,
-    getProducts: async(_: any, args: QueryPaginationArgs, context: ResolverContext) => null,
-    getCategories: async (_: any, args: QueryPaginationArgs, context: ResolverContext) => null,
-    getSuppliers: async (_: any, args: QueryPaginationArgs, context: ResolverContext) => null,
-    getEmployeeTerritories: async (_: any, args: QueryPaginationArgs, context: ResolverContext) => null
+    getOrders: (_: any, args: QueryPaginationArgs, context: ResolverContext) => getOrders(args.page),
+    getOrderDetails: (_: any, args: { id: string }, context: ResolverContext) => getOrderDtails(args.id),
+    getEmployees: (_: any, args: QueryPaginationArgs, context: ResolverContext) => null,
+    getCustomers: (_: any, args: QueryPaginationArgs, context: ResolverContext) => null,
+    getCustomerDetails: (_: any, args: { id: string }, context: ResolverContext) => null,
+    getProducts:(_: any, args: QueryPaginationArgs, context: ResolverContext) => null,
+    getCategories: (_: any, args: QueryPaginationArgs, context: ResolverContext) => null,
+    getSuppliers: (_: any, args: QueryPaginationArgs, context: ResolverContext) => null,
+    getEmployeeTerritories: (_: any, args: QueryPaginationArgs, context: ResolverContext) => null
   },
   Order: {
-    customer: async (parent: { customer_id: string }, context: ResolverContext) => null,
-    employee: async (parent: { employee_id: number }, context: ResolverContext) => null
+    customer: (parent: { customer_id: string }, context: ResolverContext) => null,
+    employee: (parent: { employee_id: number }, context: ResolverContext) => null
   },
   OrderDetail: {
-    order: async (parent: { order_id: number }, context: ResolverContext) => null,
-    product: async (parent: { product_id: number }, context: ResolverContext) => null
+    order: (parent: { order_id: number }, context: ResolverContext) => null,
+    product: (parent: { product_id: number }, context: ResolverContext) => null
   },
   EmployeeTerritory: {
-    employee: async (parent: { employee_id: number }, context: ResolverContext) => null,
-    territory: async (parent: { territory_id: number }, context: ResolverContext) => null
+    employee: (parent: { employee_id: number }, context: ResolverContext) => null,
+    territory: (parent: { territory_id: number }, context: ResolverContext) => null
   },
   Product: {
-    supplier: async (parent: { supplier_id: number }, context: ResolverContext) => null,
-    category: async (parent: { category_id: number }, context: ResolverContext) => null
+    supplier: (parent: { supplier_id: number }, context: ResolverContext) => null,
+    category: (parent: { category_id: number }, context: ResolverContext) => null
   },
   Territory: {
-    region: async (parent: { region_id: number }, context: ResolverContext) => null
+    region: (parent: { region_id: number }, context: ResolverContext) => null
   },
   Mutation: {
-    updateCustomer: async (_: any, args: CustomerMutationArgs, context: ResolverContext) => null,
-    createEmployee: async (_: any, args: CreateEmployeeMutationArgs, context: ResolverContext) => null
+    updateCustomer: (_: any, args: CustomerMutationArgs, context: ResolverContext) => null,
+    createEmployee: (_: any, args: CreateEmployeeMutationArgs, context: ResolverContext) => null
   }
 };
