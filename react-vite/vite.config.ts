9d3599f84d2332f2a2287093a2e3ba70daa07139
Chad Stewart <chadrhonanstewart@gmail.com>
2023-11-12 20:51:11 -0500

    Set up Docker image for React Vite project to build for production.

--- react-vite/vite.config.ts
+++ react-vite/vite.config.ts
@@ -5,6 +5,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+      outDir: 'build',
+      assetsDir: 'assets',
+      emptyOutDir: true
+  },
+  server:{
+    host: true,
+    strictPort: true,
+    port: 4000
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
