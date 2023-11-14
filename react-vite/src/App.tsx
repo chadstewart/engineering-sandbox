15699bfcd92344325d240e6843eb29b54a71c771
Chad Stewart <chadrhonanstewart@gmail.com>
2023-11-13 19:06:42 -0500

    Installed Storybook and added button atom as a story.

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Button } from './components/atoms/button'
+import { Button } from './components/atoms/button/button'
 import './App.css'
 
 function App() {
---
+++ react-vite/src/stories/atoms/button.stories.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/atoms/button/button";
+
+const storyConfig = {
+  title: "Design System/Atoms/Button"
+};
+
+export default storyConfig;
+
+export const ButtonStory = () => <Button>Hello</Button>;
