6cc9ca2b86c2e985be4b1fb6b26fa10f1808806a
Chad Stewart <chadrhonanstewart@gmail.com>
2023-11-28 19:23:06 -0500

    Added loading state for cats page.

--- front-end/react-vite/src/components/pages/cats.tsx
+++ front-end/react-vite/src/components/pages/cats.tsx
@@ -1,25 +1,60 @@
 import { useQuery } from "@tanstack/react-query";
 import { Card, CardContent, CardHeader } from "../ui/card";
 import { getCats } from "@/lib/api/graphql/internal-apis/cats";
+import { Skeleton } from "../ui/skeleton";
 
 const Cats = () => {
-  const { data } = useQuery({
+  const { data, isLoading, error } = useQuery({
     queryKey: ["responseData"],
     queryFn: getCats
   });
 
   return (
     <div className="flex flex-wrap gap-6 justify-center p-4">
-      {data?.getCats && data.getCats.map((cat, index) =>
-      <Card key={index} className="min-w-[250px]">
-        <CardHeader>
-          This is a test.
-        </CardHeader>
-        <CardContent>
-          <img src={cat?.url ? cat.url : undefined} alt={`Cute Cat #${index + 1}`} />
-        </CardContent>
-      </Card>
+      {isLoading &&
+        <>
+          <Card className="min-w-[250px]">
+            <CardHeader>
+              <Skeleton className="w-[100px] h-[20px] rounded-full"  />
+            </CardHeader>
+            <CardContent>
+              <Skeleton className="w-[100px] h-[200px] rounded-full"  />
+            </CardContent>
+          </Card>
+          <Card className="min-w-[250px]">
+            <CardHeader>
+              <Skeleton className="w-[100px] h-[20px] rounded-full"  />
+            </CardHeader>
+            <CardContent>
+              <Skeleton className="w-[100px] h-[200px] rounded-full"  />
+            </CardContent>
+          </Card>
+          <Card className="min-w-[250px]">
+            <CardHeader>
+              <Skeleton className="w-[100px] h-[20px] rounded-full"  />
+            </CardHeader>
+            <CardContent>
+              <Skeleton className="w-[100px] h-[200px] rounded-full"  />
+            </CardContent>
+          </Card>
+        </>
+      }
+      {data?.getCats &&
+        data.getCats.map((cat, index) =>
+          <Card key={index} className="min-w-[250px]">
+            <CardHeader>
+              This is a test.
+            </CardHeader>
+            <CardContent>
+              <img src={cat?.url ? cat.url : undefined} alt={`Cute Cat #${index + 1}`} />
+            </CardContent>
+          </Card>
       )}
+      {error && 
+        <div>
+          Well that's not good...
+        </div>
+      }
     </div>
   );
 };
