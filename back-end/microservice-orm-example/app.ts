b8c6abfa12b760bb231a5f2d4cbcfd0b31254192
Chad Stewart <chadrhonanstewart@gmail.com>
2023-12-04 21:32:14 -0500

    Set up Swagger-UI for microservice example project to use auto generated docs. Added docs auto gen to build process of docker container so container will always serve updated swagger docs.

--- back-end/microservice-orm-example/app.ts
+++ back-end/microservice-orm-example/app.ts
@@ -1,11 +1,22 @@
 import express from "express";
-import https from "https";
+/* import https from "https";
 import fs from "fs";
-import path from "path";
+import path from "path"; */
 import dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./docs/swagger_output.json";
 
 export const app = express();
 dotenv.config();
+
+//Initialize Swagger Doc Server
+const swaggerOptions = {
+    swaggerOptions: {
+        docExpansion: true
+    }
+};
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, swaggerOptions));
 
 //Initialize Request Data Type
 app.use(express.json({ limit: "10mb" }));
--- back-end/microservice-orm-example/services/server.ts
+++ back-end/microservice-orm-example/services/server.ts
@@ -2,5 +2,5 @@ import { app } from "../app";
 
 const port = 3001;
 app.listen(port, () => {
-    console.log(`Server is live on ${port}`);
+    console.log(`Server is live here: http://localhost:${port} \nView API Endpoint documentation here: http://localhost:${port}/docs`);
 });
