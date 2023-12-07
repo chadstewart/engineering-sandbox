a20cfd8b417f63089c773df15fbb84610bdc1973
Chad Stewart <chadrhonanstewart@gmail.com>
2023-12-07 09:38:06 -0500

    Added example request body to add employee.

--- back-end/microservice-orm-example/services/swagger-autogen.ts
+++ back-end/microservice-orm-example/services/swagger-autogen.ts
@@ -1,6 +1,6 @@
 import swaggerAutogen from "swagger-autogen";
 
-const outputFile = '../../docs/swagger_output.json'
-const endpointsFiles = ['../v1/routes/router.js']
+const outputFile = "../../docs/swagger_output.json";
+const endpointsFiles = ["../v1/routes/router.js"];
 
-swaggerAutogen(outputFile, endpointsFiles)
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles);
--- back-end/microservice-orm-example/v1/controllers/employees-controller.ts
+++ back-end/microservice-orm-example/v1/controllers/employees-controller.ts
@@ -2,14 +2,14 @@ import { Request, Response, NextFunction } from "express";
 import { createEmployee, employees, employeesFromId } from "../../models/employees";
 import { createEmployeeZodSchema } from "../../util/schemas/employee-zod-schema";
 
-export async function getEmployees (req: Request, res: Response, next: NextFunction) {
+export async function getEmployees(req: Request, res: Response, next: NextFunction) {
   let page = 1;
 
   const isPageNumberInRoute = req.params.page;
-  if(isPageNumberInRoute) page = Number(req.params.page);
+  if (isPageNumberInRoute) page = Number(req.params.page);
 
   const isPageNumberNaN = Number.isNaN(page);
-  if(isPageNumberNaN) {
+  if (isPageNumberNaN) {
     res.status(400).json({
       status: "failed",
       error: "employees/'page' must be a number"
@@ -26,16 +26,16 @@ export async function getEmployees (req: Request, res: Response, next: NextFunct
   });
 
   return next();
-};
+}
 
-export async function getEmployeeById (req: Request, res: Response, next: NextFunction) {
+export async function getEmployeeById(req: Request, res: Response, next: NextFunction) {
   let employeeId = 0;
 
   const isEmployeeIdInRoute = req.params.employee_id;
-  if(isEmployeeIdInRoute) employeeId = Number(req.params.employee_id);
+  if (isEmployeeIdInRoute) employeeId = Number(req.params.employee_id);
 
   const isEmployeeIdIsNaN = Number.isNaN(employeeId);
-  if(isEmployeeIdIsNaN) {
+  if (isEmployeeIdIsNaN) {
     res.status(400).json({
       status: "failed",
       error: "employees/'employeeId' must be a number"
@@ -52,14 +52,42 @@ export async function getEmployeeById (req: Request, res: Response, next: NextFu
   });
 
   return next();
-};
+}
 
-export async function addEmployee (req: Request, res: Response, next: NextFunction) {
+export async function addEmployee(req: Request, res: Response, next: NextFunction) {
+  /*  #swagger.requestBody = {
+            required: true,
+            content: {
+                "application/json": {
+                    example: {
+                      last_name: "Hello",
+                      first_name: "Hello",
+                      title: "Hello",
+                      title_of_courtesy: "Hello",
+                      birth_date: "2023/12/1",
+                      hire_date: "2023/12/1",
+                      address: "Hello",
+                      city: "Hello",
+                      region: "Hello",
+                      postal_code: "Hello",
+                      country: "Hello",
+                      home_phone: "Hello",
+                      extension: "Hello",
+                      photo: "Hello",
+                      notes: "Hello",
+                      reports_to: 1,
+                      photo_path: "Hello",
+                      territory_id: "Hello"
+                    }    
+                }
+            }
+        } 
+    */
   try {
     const addEmployeesReqBody = createEmployeeZodSchema.parse(req.body);
 
     const data = await createEmployee(addEmployeesReqBody);
-  
+
     res.status(200).json({
       status: "success",
       data: data
@@ -74,4 +102,4 @@ export async function addEmployee (req: Request, res: Response, next: NextFuncti
 
     return next();
   }
-};
+}
