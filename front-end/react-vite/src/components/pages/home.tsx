1b6588afb689a7c7edc4bdf39b5b741144693f56
Chad Stewart <chadrhonanstewart@gmail.com>
2023-12-07 20:31:35 -0500

    Made some minor progress on how the home page is going to look.

--- front-end/react-vite/src/components/pages/home.tsx
+++ front-end/react-vite/src/components/pages/home.tsx
@@ -4,23 +4,19 @@ const Home = () => {
   return (
     <div className="flex flex-wrap gap-6 justify-center p-4">
       <Card className="min-w-[250px]">
-        <CardHeader>This is a test.</CardHeader>
+        <CardHeader>Orders</CardHeader>
         <CardContent>This is still a test!</CardContent>
       </Card>
       <Card className="min-w-[250px]">
-        <CardHeader>This is a test.</CardHeader>
+        <CardHeader>Sales</CardHeader>
         <CardContent>This is still a test!</CardContent>
       </Card>
       <Card className="min-w-[250px]">
-        <CardHeader>This is a test.</CardHeader>
+        <CardHeader>Customers</CardHeader>
         <CardContent>This is still a test!</CardContent>
       </Card>
       <Card className="min-w-[250px]">
-        <CardHeader>This is a test.</CardHeader>
-        <CardContent>This is still a test!</CardContent>
-      </Card>
-      <Card className="min-w-[250px]">
-        <CardHeader>This is a test.</CardHeader>
+        <CardHeader>Products</CardHeader>
         <CardContent>This is still a test!</CardContent>
       </Card>
     </div>
