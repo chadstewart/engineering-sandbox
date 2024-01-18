3cd5010c4af01ddffd6f882420f469d9694648c3
Chad Stewart <chadrhonanstewart@gmail.com>
2024-01-17 20:07:47 -0500

    Got login and sign up working. Had to fix a few issues with my implementation.

--- front-end/react-vite/src/components/molecules/login/login.tsx
+++ front-end/react-vite/src/components/molecules/login/login.tsx
@@ -1,13 +1,38 @@
 import { Button } from "@/components/atoms/button/button";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const Login = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: "/profile"
+      },
+      authorizationParams: {
+        prompt: "login"
+      }
+    });
+  };
+  const handleSignUp = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: "/profile"
+      },
+      authorizationParams: {
+        prompt: "login",
+        screen_hint: "signup"
+      }
+    });
+  };
+
   return (
     <div className="flex justify-between">
       <span>
-        <Button>Sign up</Button>
+        <Button onClick={handleSignUp}>Sign up</Button>
       </span>
       <span>
-        <Button>Login</Button>
+        <Button onClick={handleLogin}>Login</Button>
       </span>
     </div>
   );
--- front-end/react-vite/src/components/particle/auth0-provider/auth0-provider-with-navigate.tsx
+++ front-end/react-vite/src/components/particle/auth0-provider/auth0-provider-with-navigate.tsx
@@ -1,20 +1,13 @@
 import { Auth0Provider } from "@auth0/auth0-react";
-import { useNavigate } from "@tanstack/react-router";
 
 interface Auth0ProviderWithNavigateProps {
   children: React.ReactNode;
 }
 
 export const Auth0ProviderWithNavigate = ({ children }: Auth0ProviderWithNavigateProps) => {
-  const navigate = useNavigate();
-
-  const domain = process.env.VITE_AUTH0_URL;
-  const clientId = process.env.VITE_AUTH0_CLIENT_ID;
-  const redirectUri = process.env.VITE_AUTH0_CALLBACK_URL;
-
-  const onRedirectCallback = () => {
-    navigate({ to: "/" });
-  };
+  const domain = import.meta.env.VITE_AUTH0_URL;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
 
   if (!(domain && clientId && redirectUri)) {
     return null;
@@ -27,7 +20,6 @@ export const Auth0ProviderWithNavigate = ({ children }: Auth0ProviderWithNavigat
       authorizationParams={{
         redirect_uri: redirectUri
       }}
-      onRedirectCallback={onRedirectCallback}
     >
       {children}
     </Auth0Provider>
