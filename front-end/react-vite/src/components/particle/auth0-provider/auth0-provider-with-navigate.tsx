26463362bdea800fdc94744366b96d6a3428939d
Chad Stewart <chadrhonanstewart@gmail.com>
2024-01-17 19:51:03 -0500

    Hehe, forgot to change my .env variables to use vite.

--- front-end/react-vite/src/components/particle/auth0-provider/auth0-provider-with-navigate.tsx
+++ front-end/react-vite/src/components/particle/auth0-provider/auth0-provider-with-navigate.tsx
@@ -8,9 +8,9 @@ interface Auth0ProviderWithNavigateProps {
 export const Auth0ProviderWithNavigate = ({ children }: Auth0ProviderWithNavigateProps) => {
   const navigate = useNavigate();
 
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+  const domain = process.env.VITE_AUTH0_URL;
+  const clientId = process.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = process.env.VITE_AUTH0_CALLBACK_URL;
 
   const onRedirectCallback = () => {
     navigate({ to: "/" });
