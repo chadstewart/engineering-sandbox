e6b8eae4feb5a775c83b8c24d00f839e978a8118
Chad Stewart <chadrhonanstewart@gmail.com>
2024-01-25 23:27:58 -0500

    Fixed issue where params was throwing an error because of mismatched types.

--- front-end/react-vite/src/components/atoms/nav-item/nav-item.tsx
+++ front-end/react-vite/src/components/atoms/nav-item/nav-item.tsx
@@ -5,7 +5,12 @@ import { Link } from "@tanstack/react-router";
 
 export const NavItem = ({ url, params, text, icon, iconAlt, externalLinkAlt }: NavItemProps) => {
   return (
-    <Link className="flex gap-2 w-full m-2" to={url} params={params} target={externalLinkAlt ? "_blank" : "_self"}>
+    <Link
+      className="flex gap-2 w-full m-2"
+      to={url}
+      params={params ? params : {}}
+      target={externalLinkAlt ? "_blank" : "_self"}
+    >
       {icon && <Icon Icon={icon} iconAlt={iconAlt} />}
       <div className="font-bold">{text}</div>
       {externalLinkAlt && <Icon Icon={RiExternalLinkLine} iconAlt={externalLinkAlt} />}
