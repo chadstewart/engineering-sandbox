13c8a616a2fe265a6a688912d3b878bba618b12f
Chad Stewart <chadrhonanstewart@gmail.com>
2024-01-31 21:12:46 -0500

    Made sidebar sticky on desktop side menu.

--- front-end/react-vite/src/components/organisms/sidebar/sidebar.tsx
+++ front-end/react-vite/src/components/organisms/sidebar/sidebar.tsx
@@ -3,7 +3,7 @@ import { SidebarContent } from "../../molecules/sidebar-content/sidebar-content"
 
 const Sidebar = () => {
   return (
-    <aside className="flex flex-col p-3 w-96 border-r-2">
+    <aside className="flex flex-col h-screen sticky top-0 p-3 w-96 border-r-2">
       <SidebarHeader />
       <SidebarContent />
     </aside>
