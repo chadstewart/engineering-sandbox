5f2f88c22eabd6772e920fc8bf043236be1f8736
Chad Stewart <chadrhonanstewart@gmail.com>
2024-06-05 17:02:10 -0500

    Added test suite for region model.

---
+++ back-end/monolithic-api/test/models/region.test.ts
@@ -0,0 +1,36 @@
+import { expect, describe, it, vi, afterEach } from "vitest";
+import { prisma } from "../../services/database";
+import { regionDetails } from "../../models/region";
+
+describe("Models: Regions", () => {
+  vi.mock("../../services/database", () => {
+    return {
+      prisma: {
+        region: {
+          findMany: vi.fn(() => "test"),
+          count: vi.fn(() => 1)
+        }
+      }
+    };
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it("regionDetails: Should return data in the expected shape", async () => {
+    const outputToTest = await regionDetails();
+    expect(outputToTest).toBe("test");
+  });
+
+  it("regionDetails: Should call prismaPagiantionHelper with appropriate page number", async () => {
+    const testCategoryId = 2;
+    const mockFindManyFn = vi.mocked(prisma.region.findMany);
+    await regionDetails(testCategoryId);
+    expect(mockFindManyFn).toHaveBeenCalledWith({
+      where: {
+        region_id: testCategoryId
+      }
+    });
+  });
+});
