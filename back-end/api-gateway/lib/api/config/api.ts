38146216bfbeb951cea847afc2c2e3c341f4cca2
Chad Stewart <chadrhonanstewart@gmail.com>
2024-06-10 20:06:40 -0500

    Updated node type definitions for monlithic api and api gateway. Specifically needed for api gateway because it had to use the DOM's version of fetch before definitely typed release type definitions for node's fetch.

--- back-end/api-gateway/lib/api/config/api.ts
+++ back-end/api-gateway/lib/api/config/api.ts
@@ -2,34 +2,34 @@ import { createZodFetcher } from "zod-fetch";
 import { exponentialBackOff } from "../util/exponential-back-off";
 
 const api = {
-  get: createZodFetcher((url: URL | RequestInfo, config: RequestInit = {}) =>
+  get: createZodFetcher((url: string | URL | Request, config: RequestInit = {}) =>
     exponentialBackOff(fetch)(url, {
       method: "GET",
       ...config
     })
   ),
-  post: createZodFetcher((url: URL | RequestInfo, body: unknown, config: RequestInit = {}) =>
+  post: createZodFetcher((url: string | URL | Request, body: unknown, config: RequestInit = {}) =>
     exponentialBackOff(fetch)(url, {
       method: "POST",
       body: JSON.stringify(body),
       ...config
     })
   ),
-  put: createZodFetcher((url: URL | RequestInfo, body: unknown, config: RequestInit = {}) =>
+  put: createZodFetcher((url: string | URL | Request, body: unknown, config: RequestInit = {}) =>
     exponentialBackOff(fetch)(url, {
       method: "PUT",
       body: JSON.stringify(body),
       ...config
     })
   ),
-  patch: createZodFetcher((url: URL | RequestInfo, body: unknown, config: RequestInit = {}) =>
+  patch: createZodFetcher((url: string | URL | Request, body: unknown, config: RequestInit = {}) =>
     exponentialBackOff(fetch)(url, {
       method: "PATCH",
       body: JSON.stringify(body),
       ...config
     })
   ),
-  delete: createZodFetcher((url: URL | RequestInfo, config: RequestInit = {}) =>
+  delete: createZodFetcher((url: string | URL | Request, config: RequestInit = {}) =>
     exponentialBackOff(fetch)(url, {
       method: "DELETE",
       ...config
