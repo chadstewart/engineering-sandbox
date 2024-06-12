78b031062521768be728c6e004462545c4c4f824
Chad Stewart <chadrhonanstewart@gmail.com>
2024-06-12 18:28:55 -0500

    Fixed issue where pages on desktop views where not full sized to the screen

--- front-end/react-vite/src/components/templates/layout/page/index.tsx
+++ front-end/react-vite/src/components/templates/layout/page/index.tsx
@@ -4,7 +4,7 @@ export const PageLayout = () => {
   const pageName = document.location.href.split("/")[3];
 
   return (
-    <div className="px-2">
+    <div className="px-2 w-full">
       <header className="hidden xl:block capitalize py-2 border-b-2">
         <h2 className="font-semibold">{pageName && !pageName.includes("redirect") ? pageName : "Home"}</h2>
       </header>
