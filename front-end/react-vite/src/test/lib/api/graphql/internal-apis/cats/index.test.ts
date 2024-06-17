9fc72b58feec96913ef2d91ae8515e83b266b16b
Chad Stewart <chadrhonanstewart@gmail.com>
2024-06-17 17:49:03 -0500

    Added test suite for front-end graphql api call for cat query.

---
+++ front-end/react-vite/src/test/lib/api/graphql/internal-apis/cats/index.test.ts
@@ -0,0 +1,47 @@
+import { beforeEach, it, describe, expect, vi } from "vitest";
+import { graphql } from "@/gql";
+import api from "@/lib/api/config/api";
+import { getCats } from "@/lib/api/graphql/internal-apis/cats";
+
+describe("GraphQL Internal API Function: Cats", () => {
+  beforeEach(() => {
+    vi.mock("@/lib/api/config/api", () => {
+      return {
+        default: {
+          graphqlQuery: vi.fn(() => "test")
+        }
+      };
+    });
+
+    vi.mock("@/gql", () => {
+      return {
+        graphql: vi.fn(() => "test")
+      };
+    });
+
+    vi.stubEnv("VITE_API_GATEWAY_URL", "test");
+  });
+
+  it("getCats: Should call graphQLQuery and graphQL functions with appropriate arguments", async () => {
+    const mockGraphQLQueryFn = vi.mocked(api.graphqlQuery);
+    const mockGraphQLFn = vi.mocked(graphql);
+    await getCats();
+    expect(mockGraphQLQueryFn).toHaveBeenCalledWith("test", "test", { getDog: false });
+    expect(mockGraphQLFn).toHaveBeenCalledWith(`
+      query GetCats($getDog: Boolean) {
+        getCats(getDog: $getDog) {
+          url
+          width
+          height
+        }
+      }
+    `);
+  });
+
+  it("getCats: Should call graphQLQuery function with getDog being true", async () => {
+    const variableToTest = true;
+    const mockGraphQLQueryFn = vi.mocked(api.graphqlQuery);
+    await getCats(variableToTest);
+    expect(mockGraphQLQueryFn).toHaveBeenCalledWith("test", "test", { getDog: variableToTest });
+  });
+});
