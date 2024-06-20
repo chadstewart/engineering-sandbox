24c81ead0f0175a81072122a445778b6df9601c4
Chad Stewart <chadrhonanstewart@gmail.com>
2024-06-20 18:43:37 -0500

    Updated auth0 particle to show outputted component and what it's getting passed.

--- front-end/react-vite/src/test/components/particles/auth0-provider/auth0-provider-with-navigate.test.tsx
+++ front-end/react-vite/src/test/components/particles/auth0-provider/auth0-provider-with-navigate.test.tsx
@@ -3,14 +3,19 @@
  */
 
 import { Auth0ProviderWithNavigate } from "@/components/particle/auth0-provider/auth0-provider-with-navigate";
-import { render } from "@testing-library/react";
+import { renderHook } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
 describe("Particle Component: Auth0ProviderWithNavigate", () => {
+  import.meta.env.VITE_AUTH0_URL = "test";
+  import.meta.env.VITE_AUTH0_CLIENT_ID = "test";
+  import.meta.env.VITE_AUTH0_CALLBACK_URL = "test";
+  import.meta.env.VITE_AUTH0_AUDIENCE = "test";
+
   it("Should match the snapshot", () => {
     const {
-      container: { firstChild: variableToSnapshot }
-    } = render(<Auth0ProviderWithNavigate children={<div></div>} />);
+      result: { current: variableToSnapshot }
+    } = renderHook(() => Auth0ProviderWithNavigate({ children: <div></div> }));
 
     expect(variableToSnapshot).toMatchSnapshot();
   });
