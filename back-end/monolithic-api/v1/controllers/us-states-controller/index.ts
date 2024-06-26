daea8c719bbf82b5ba2ea0b3229f01d4820c8cc5
Chad Stewart <chadrhonanstewart@gmail.com>
2024-06-25 19:02:31 -0500

    Added typing to request params (which I didn't know I could do XD).

--- back-end/monolithic-api/v1/controllers/us-states-controller/index.ts
+++ back-end/monolithic-api/v1/controllers/us-states-controller/index.ts
@@ -3,7 +3,7 @@ import { handleUsStatesRequest } from "./util/handle-us-states-request";
 import { parseUsStatesRequest } from "./util/parse-us-states-request";
 import { usStates } from "../../../models/us-states";
 
-export async function getUSStates(req: Request, res: Response, next: NextFunction) {
+export async function getUSStates(req: Request<{ page: string }>, res: Response, next: NextFunction) {
   const { statusCode, ...rest } = await handleUsStatesRequest(parseUsStatesRequest(req), usStates);
   const usStatesResponse = rest;
   res.status(statusCode).json(usStatesResponse);
--- back-end/monolithic-api/v1/controllers/us-states-controller/util/parse-us-states-request.ts
+++ back-end/monolithic-api/v1/controllers/us-states-controller/util/parse-us-states-request.ts
@@ -2,6 +2,6 @@ import { Request } from "express";
 import { parsePaginationRequest } from "../../../../util/pagination-utils/parse-pagination-request";
 import { UsStatesEvaluatedRequest } from "./types/us-states-types";
 
-export const parseUsStatesRequest = (req: Request): UsStatesEvaluatedRequest => {
+export const parseUsStatesRequest = (req: Request<{ page: string }>): UsStatesEvaluatedRequest => {
   return parsePaginationRequest(req);
 };
