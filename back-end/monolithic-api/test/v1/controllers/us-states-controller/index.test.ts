ecb8e85bfba9ac16370443c3567be428977b3b47
Chad Stewart <chadrhonanstewart@gmail.com>
2024-06-25 20:36:43 -0500

    Replaced previous test suite for us states controller with new implementation.

---
+++ back-end/monolithic-api/test/v1/controllers/us-states-controller/index.test.ts
@@ -0,0 +1,64 @@
+import { describe, expect, it, vi } from "vitest";
+import { getUSStates } from "../../../../v1/controllers/us-states-controller";
+import { afterEach, beforeEach } from "node:test";
+import { Request, Response } from "express";
+
+describe("Controller: US States", () => {
+  beforeEach(() => {
+    vi.mock("../../../../models/us-states", () => {
+      return {
+        usStates: vi.fn(() => "test")
+      };
+    });
+
+    vi.mock("../../../../v1/controllers/us-states-controller/util/handle-us-states-request", () => {
+      return {
+        handleUsStatesRequest: vi.fn(() => {
+          return {
+            statusCode: 200,
+            data: "test"
+          };
+        })
+      };
+    });
+
+    vi.mock("../../../../v1/controllers/us-states-controller/util/parse-us-states-request", () => {
+      return {
+        parseUsStatesRequest: vi.fn(() => "test")
+      };
+    });
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+  it("Should send a response", async () => {
+    const mockRequest = {
+      params: {
+        page: 1
+      }
+    };
+
+    const mockNextFunc = vi.fn();
+    const mockJsonFunc = vi.fn();
+    const mockStatusFunc = vi.fn(() => {
+      return {
+        json: mockJsonFunc
+      };
+    });
+    const mockResponse = {
+      status: mockStatusFunc
+    };
+
+    await getUSStates(
+      mockRequest as unknown as Request<{ page: string }>,
+      mockResponse as unknown as Response,
+      mockNextFunc
+    );
+
+    expect(mockStatusFunc).toHaveBeenCalledWith(200);
+    expect(mockJsonFunc).toHaveBeenCalledWith({
+      data: "test"
+    });
+  });
+});
