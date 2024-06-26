bfb8c9a1be734458b18847d78c2c8cffd6bf6592
Chad Stewart <chadrhonanstewart@gmail.com>
2024-06-25 19:14:45 -0500

    Added test suite for us state controller util function create error message.

---
+++ back-end/monolithic-api/test/v1/controllers/us-states-controller/util/create-error-message.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from "vitest";
+import { createErrorMessage } from "../../../../../v1/controllers/us-states-controller/util/create-error-message";
+import { UsStatesRequestError } from "../../../../../v1/controllers/us-states-controller/util/types/us-states-types";
+
+describe("US States Controller Util: Create Error Message", () => {
+  it("Should output 'parameter 'page' in states/'page' is missing' when error object is 'MissingPage'", () => {
+    const testObj: UsStatesRequestError = {
+      error: "MissingPage"
+    };
+    const variableToTest = createErrorMessage(testObj);
+    expect(variableToTest).toBe("parameter 'page' in states/'page' is missing");
+  });
+
+  it("Should output 'states/'page' must be a number' when error object is 'PageIsNotAValidNumber'", () => {
+    const testObj: UsStatesRequestError = {
+      error: "PageIsNotAValidNumber"
+    };
+    const variableToTest = createErrorMessage(testObj);
+    expect(variableToTest).toBe("states/'page' must be a number");
+  });
+});
