57b346d86d9b69d5c0d7fa9c09e20dc975bd1a8e
Chad Stewart <chadrhonanstewart@gmail.com>
2024-06-26 20:22:24 -0500

    Completed refactor of revenue controller.

---
+++ back-end/monolithic-api/v1/controllers/revenue-controller/index.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from "express";
+import { handleRevenueRequest } from "./util/handle-revenue-request";
+import { totalRevenue } from "../../../models/revenue";
+
+export async function getTotalRevenue(req: Request, res: Response, next: NextFunction) {
+  const { statusCode, ...rest } = await handleRevenueRequest(totalRevenue);
+  const revenueRequest = rest;
+  res.status(statusCode).json(revenueRequest);
+  return next();
+}
