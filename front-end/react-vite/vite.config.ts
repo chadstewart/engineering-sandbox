71df83fd9532f8a4a7c8ffc05baf038f5987b0cc
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-08 19:22:01 -0500

    Added gzip compression to vite build. Updated nginx to serve compressed files when able.

--- front-end/react-vite/vite.config.ts
+++ front-end/react-vite/vite.config.ts
@@ -1,23 +1,24 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCompression()],
   build: {
-      outDir: 'build',
-      assetsDir: 'assets',
-      emptyOutDir: true
+    outDir: "build",
+    assetsDir: "assets",
+    emptyOutDir: true
   },
-  server:{
+  server: {
     host: true,
     strictPort: true,
     port: 4001
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
+});
