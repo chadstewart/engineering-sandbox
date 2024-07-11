9cdb5baaf08cac786a6a8176e4e03b56cf128288
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-11 18:45:22 -0500

    Completed refactor of getCategories from categories controller.

--- back-end/monolithic-api/v1/controllers/categories-controller/index.ts
+++ back-end/monolithic-api/v1/controllers/categories-controller/index.ts
@@ -1,29 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import { categories, categoryDetails } from "../../../models/categories";
+import { handleGetCategoriesRequest } from "./util/get-categories/handle-get-categories-request";
+import { parseGetCategoriesRequest } from "./util/get-categories/parse-get-categories-request";
+import { GetCategoriesParams } from "./util/get-categories/types/get-categories-types";
 
-export async function getCategories(req: Request, res: Response, next: NextFunction) {
-  let page = 1;
-
-  const isPageNumberInRoute = req.params.page;
-  if (isPageNumberInRoute) page = Number(req.params.page);
-
-  const isPageNumberNaN = Number.isNaN(page);
-  if (isPageNumberNaN) {
-    res.status(400).json({
-      status: "failed",
-      error: "category/'page' must be a number"
-    });
-
-    return next();
-  }
-
-  const data = await categories(page);
-
-  res.status(200).json({
-    status: "success",
-    data: data
-  });
-
+export async function getCategories(req: Request<GetCategoriesParams>, res: Response, next: NextFunction) {
+  const { statusCode, ...rest } = await handleGetCategoriesRequest(parseGetCategoriesRequest(req), categories, req);
+  const getCategoriesResponse = rest;
+  res.status(statusCode).json(getCategoriesResponse);
   return next();
 }
 
