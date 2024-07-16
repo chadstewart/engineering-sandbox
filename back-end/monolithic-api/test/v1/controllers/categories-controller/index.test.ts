ab2e79aedf9dae72822b0e74233e0a959f75b031
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-15 20:05:33 -0500

    Refactored category controller tests.

--- back-end/monolithic-api/test/v1/controllers/categories-controller/index.test.ts
+++ back-end/monolithic-api/test/v1/controllers/categories-controller/index.test.ts
@@ -1,12 +1,16 @@
 import { describe, expect, it, vi } from "vitest";
-import { getCategories } from "../../../../v1/controllers/categories-controller/index";
+import { getCategories, getCategoryDetails } from "../../../../v1/controllers/categories-controller/index";
 import { Request, Response } from "express";
-import { GetCategoriesParams } from "../../../../v1/controllers/categories-controller/util/types/categories-types";
+import {
+  GetCategoriesParams,
+  GetCategoryDetailsParams
+} from "../../../../v1/controllers/categories-controller/util/types/categories-types";
 
 describe("Controller: Categories", () => {
   vi.mock("../../../../models/categories", () => {
     return {
-      categories: vi.fn(() => "test")
+      categories: vi.fn(() => "test"),
+      categoryDetails: vi.fn(() => "test")
     };
   });
 
@@ -26,6 +30,29 @@ describe("Controller: Categories", () => {
       parseGetCategoriesRequest: vi.fn(() => "test")
     };
   });
+
+  vi.mock(
+    "../../../../v1/controllers/categories-controller/util/get-categories-details/handle-get-category-details-request",
+    () => {
+      return {
+        handleGetCategoryDetailsRequest: vi.fn(() => {
+          return {
+            statusCode: 200,
+            data: "test"
+          };
+        })
+      };
+    }
+  );
+
+  vi.mock(
+    "../../../../v1/controllers/categories-controller/util/get-categories-deatils/parse-get-category-details-request",
+    () => {
+      return {
+        parseGetCategoryDetailsRequest: vi.fn(() => "test")
+      };
+    }
+  );
 
   it("getCategories: Should send a response", async () => {
     const mockRequest = {
@@ -77,6 +104,64 @@ describe("Controller: Categories", () => {
 
     await getCategories(
       mockRequest as unknown as Request<GetCategoriesParams>,
+      mockResponse as unknown as Response,
+      mockNextFunc
+    );
+
+    expect(mockNextFunc).toHaveBeenCalled();
+  });
+
+  it("getCategoryDetails: Should send a response", async () => {
+    const mockRequest = {
+      params: {
+        category_id: 1
+      }
+    };
+
+    const mockNextFunc = vi.fn();
+    const mockJsonFunc = vi.fn();
+    const mockStatusFunc = vi.fn(() => {
+      return {
+        json: mockJsonFunc
+      };
+    });
+    const mockResponse = {
+      status: mockStatusFunc
+    };
+
+    await getCategoryDetails(
+      mockRequest as unknown as Request<GetCategoryDetailsParams>,
+      mockResponse as unknown as Response,
+      mockNextFunc
+    );
+
+    expect(mockStatusFunc).toHaveBeenCalledWith(200);
+    expect(mockJsonFunc).toHaveBeenCalledWith({
+      status: "success",
+      data: "test"
+    });
+  });
+
+  it("getCategories: Should call the next function", async () => {
+    const mockRequest = {
+      params: {
+        category_id: 1
+      }
+    };
+
+    const mockNextFunc = vi.fn();
+    const mockJsonFunc = vi.fn();
+    const mockStatusFunc = vi.fn(() => {
+      return {
+        json: mockJsonFunc
+      };
+    });
+    const mockResponse = {
+      status: mockStatusFunc
+    };
+
+    await getCategoryDetails(
+      mockRequest as unknown as Request<GetCategoryDetailsParams>,
       mockResponse as unknown as Response,
       mockNextFunc
     );
