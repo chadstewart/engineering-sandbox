87bc5b893a0a7c427a37ff166621c95726c7f220
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-18 17:55:40 -0500

    Completed refactor of get supplier details controller function.

--- back-end/monolithic-api/v1/controllers/suppliers-controller/index.ts
+++ back-end/monolithic-api/v1/controllers/suppliers-controller/index.ts
@@ -2,7 +2,9 @@ import { Request, Response, NextFunction } from "express";
 import { supplier, supplierDetails } from "../../../models/suppliers";
 import { handleGetSupplierRequest } from "./util/get-suppliers/handle-get-suppliers-request";
 import { parseGetSupplierRequest } from "./util/get-suppliers/parse-get-suppliers-request";
-import { GetSuppliersParams } from "./util/types/supplier-types";
+import { GetSupplierDetailsParams, GetSuppliersParams } from "./util/types/supplier-types";
+import { handleGetSupplierDetailsRequest } from "./util/get-supplier-details/handle-get-supplier-details-request";
+import { parseGetSuppliersDetailsRequest } from "./util/get-supplier-details/parse-get-supplier-details-request";
 
 export async function getSuppliers(req: Request<GetSuppliersParams>, res: Response, next: NextFunction) {
   const { statusCode, ...rest } = await handleGetSupplierRequest(parseGetSupplierRequest(req), supplier, req.path);
@@ -11,28 +13,12 @@ export async function getSuppliers(req: Request<GetSuppliersParams>, res: Respon
   return next();
 }
 
-export async function getSupplierDetails(req: Request, res: Response, next: NextFunction) {
-  let supplierId = 1;
-
-  const isSupplierIdInRoute = req.params.supplier_id;
-  if (isSupplierIdInRoute) supplierId = Number(req.params.supplier_id);
-
-  const isSupplierIdNaN = Number.isNaN(supplierId) || supplierId === 1;
-  if (isSupplierIdNaN) {
-    res.status(400).json({
-      status: "failed",
-      error: "supplier/details/'supplier_id' must be a number"
-    });
-
-    return next();
-  }
-
-  const data = await supplierDetails(supplierId);
-
-  res.status(200).json({
-    status: "success",
-    data: data
-  });
-
+export async function getSupplierDetails(req: Request<GetSupplierDetailsParams>, res: Response, next: NextFunction) {
+  const { statusCode, ...rest } = await handleGetSupplierDetailsRequest(
+    parseGetSuppliersDetailsRequest(req),
+    supplierDetails
+  );
+  const supplierDetailsResponse = rest;
+  res.status(statusCode).json(supplierDetailsResponse);
   return next();
 }
