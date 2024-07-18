6556dc1e4325fbe0b4c05d3c9576ee0898e415a0
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-17 19:28:08 -0500

    Completed refactor of get suppliers controller function.

--- back-end/monolithic-api/v1/controllers/suppliers-controller/index.ts
+++ back-end/monolithic-api/v1/controllers/suppliers-controller/index.ts
@@ -1,29 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import { supplier, supplierDetails } from "../../../models/suppliers";
+import { handleGetSupplierRequest } from "./util/get-suppliers/handle-get-suppliers-request";
+import { parseGetSupplierRequest } from "./util/get-suppliers/parse-get-suppliers-request";
+import { GetSuppliersParams } from "./util/types/supplier-types";
 
-export async function getSuppliers(req: Request, res: Response, next: NextFunction) {
-  let page = 1;
-
-  const isPageNumberInRoute = req.params.page;
-  if (isPageNumberInRoute) page = Number(req.params.page);
-
-  const isPageNumberNaN = Number.isNaN(page);
-  if (isPageNumberNaN) {
-    res.status(400).json({
-      status: "failed",
-      error: "supplier/'page' must be a number"
-    });
-
-    return next();
-  }
-
-  const data = await supplier(page);
-
-  res.status(200).json({
-    status: "success",
-    data: data
-  });
-
+export async function getSuppliers(req: Request<GetSuppliersParams>, res: Response, next: NextFunction) {
+  const { statusCode, ...rest } = await handleGetSupplierRequest(parseGetSupplierRequest(req), supplier, req.path);
+  const supplierResponse = rest;
+  res.status(statusCode).json(supplierResponse);
   return next();
 }
 
