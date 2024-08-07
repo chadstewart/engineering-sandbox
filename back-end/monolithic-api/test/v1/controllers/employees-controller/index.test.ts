1ad407a3e42f9180b0ef3176c9d95e449e98cc14
Chad Stewart <chadrhonanstewart@gmail.com>
2024-08-07 17:35:59 -0500

    Completed refactor of test suite for get employees controller function.

--- back-end/monolithic-api/test/v1/controllers/employees-controller/index.test.ts
+++ back-end/monolithic-api/test/v1/controllers/employees-controller/index.test.ts
@@ -1,23 +1,57 @@
-import { expect, it, describe, vi, afterEach, beforeEach } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { Request, Response } from "express";
-import { addEmployee, getEmployeeById, getEmployees } from "../../../../v1/controllers/employees-controller";
+import { getEmployees } from "../../../../v1/controllers/employees-controller/";
+import { GetEmployeesParams } from "../../../../v1/controllers/employees-controller/util/types/employee-types";
 
 describe("Controller: Employees", () => {
-  beforeEach(() => {
-    vi.mock("../../../models/employees", () => {
+  vi.mock("../../../../models/employees", () => {
+    return {
+      employees: vi.fn(() => "test"),
+      employeesFromId: vi.fn(() => "test")
+    };
+  });
+
+  vi.mock("../../../../v1/controllers/employees-controller/util/get-employees/handle-get-employee-request", () => {
+    return {
+      handleGetEmployeesRequest: vi.fn(() => {
+        return {
+          statusCode: 200,
+          data: "test"
+        };
+      })
+    };
+  });
+
+  vi.mock("../../../../v1/controllers/employees-controller/util/get-employees/parse-get-employees-request", () => {
+    return {
+      parseGetEmployeesRequest: vi.fn(() => "test")
+    };
+  });
+
+  vi.mock(
+    "../../../../v1/controllers/employees-controller/util/get-employees-details/handle-get-employee-details-request",
+    () => {
       return {
-        employees: () => "test",
-        employeesFromId: () => "test",
-        createEmployee: () => "test"
+        handleGetemployeeDetailsRequest: vi.fn(() => {
+          return {
+            statusCode: 200,
+            data: "test"
+          };
+        })
       };
-    });
-  });
+    }
+  );
 
-  afterEach(() => {
-    vi.resetAllMocks();
-  });
+  vi.mock(
+    "../../../../v1/controllers/employees-controller/util/get-employees-details/parse-get-employee-details-request",
+    () => {
+      return {
+        parseGetemployeeDetailsRequest: vi.fn(() => "test")
+      };
+    }
+  );
 
-  it("getEmployees: Should send proper data response when called", async () => {
+  it("getEmployees: Should send a response", async () => {
     const mockRequest = {
       params: {
         page: 1
@@ -35,12 +69,16 @@ describe("Controller: Employees", () => {
       status: mockStatusFunc
     };
 
-    await getEmployees(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
+    await getEmployees(
+      mockRequest as unknown as Request<GetEmployeesParams>,
+      mockResponse as unknown as Response,
+      mockNextFunc
+    );
 
     expect(mockStatusFunc).toHaveBeenCalledWith(200);
     expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "success",
-      data: "test"
+      data: "test",
+      status: "success"
     });
   });
 
@@ -62,227 +100,12 @@ describe("Controller: Employees", () => {
       status: mockStatusFunc
     };
 
-    await getEmployees(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
+    await getEmployees(
+      mockRequest as unknown as Request<GetEmployeesParams>,
+      mockResponse as unknown as Response,
+      mockNextFunc
+    );
 
     expect(mockNextFunc).toHaveBeenCalled();
-  });
-
-  it("getEmployees: Should return a response object that says it failed because page isn't a number", async () => {
-    const mockRequest = {
-      params: {
-        page: "hello"
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await getEmployees(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(400);
-    expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "failed",
-      error: "employees/'page' must be a number"
-    });
-  });
-
-  it("getEmployeeById: Should send proper data response when called", async () => {
-    const mockRequest = {
-      params: {
-        employee_id: 2
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await getEmployeeById(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(200);
-    expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "success",
-      data: "test"
-    });
-  });
-
-  it("getEmployeeById: Should call the next function", async () => {
-    const mockRequest = {
-      params: {
-        employee_id: 2
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await getEmployeeById(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockNextFunc).toHaveBeenCalled();
-  });
-
-  it("getEmployeeById: Should return a response object that says it failed because page isn't a number", async () => {
-    const mockRequest = {
-      params: {
-        employee_id: "hello"
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await getEmployeeById(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(400);
-    expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "failed",
-      error: "employees/'employeeId' must be a number"
-    });
-  });
-
-  it("addEmployee: Should send proper data response when called", async () => {
-    const mockRequest = {
-      body: {
-        last_name: "Hello",
-        first_name: "Hello",
-        title: "Hello",
-        title_of_courtesy: "Hello",
-        birth_date: "2023/12/1",
-        hire_date: "2023/12/1",
-        address: "Hello",
-        city: "Hello",
-        region: "Hello",
-        postal_code: "Hello",
-        country: "Hello",
-        home_phone: "Hello",
-        extension: "Boo",
-        photo: "Hello",
-        notes: "Hello",
-        reports_to: 1,
-        photo_path: "Hello",
-        territory_id: "06897"
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await addEmployee(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(201);
-    expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "success",
-      data: "test"
-    });
-  });
-
-  it("addEmployee: Should call the next function", async () => {
-    const mockRequest = {
-      body: {
-        last_name: "Hello",
-        first_name: "Hello",
-        title: "Hello",
-        title_of_courtesy: "Hello",
-        birth_date: "2023/12/1",
-        hire_date: "2023/12/1",
-        address: "Hello",
-        city: "Hello",
-        region: "Hello",
-        postal_code: "Hello",
-        country: "Hello",
-        home_phone: "Hello",
-        extension: "Boo",
-        photo: "Hello",
-        notes: "Hello",
-        reports_to: 1,
-        photo_path: "Hello",
-        territory_id: "06897"
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await addEmployee(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(201);
-    expect(mockNextFunc).toHaveBeenCalled();
-
-    const mockBadRequest = {};
-
-    await addEmployee(mockBadRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(400);
-    expect(mockNextFunc).toHaveBeenCalled();
-  });
-
-  it("addEmployee: Should return a response object that says it failed the body object isn't correct", async () => {
-    const mockRequest = {
-      params: {
-        employee_id: "hello"
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await addEmployee(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(400);
   });
 });
