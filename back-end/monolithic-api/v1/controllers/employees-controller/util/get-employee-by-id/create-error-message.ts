e38dd4fe39708018e16cf0c86437c85976bfc706
Chad Stewart <chadrhonanstewart@gmail.com>
2024-08-27 17:27:40 -0500

    Added createErrorMessage util function for get employee by id controller function.

---
+++ back-end/monolithic-api/v1/controllers/employees-controller/util/get-employee-by-id/create-error-message.ts
@@ -0,0 +1,7 @@
+import { GetEmployeeByIdRequestError } from "../types/employee-types";
+
+export const createErrorMessage = (evaluatedRequest: GetEmployeeByIdRequestError) => {
+  if (evaluatedRequest.error === "MissingEmployeeId")
+    return "parameter 'employee_id' in employee/details/'employee_id' is missing";
+  return "employee/details/'employee_id' must be a number";
+};
