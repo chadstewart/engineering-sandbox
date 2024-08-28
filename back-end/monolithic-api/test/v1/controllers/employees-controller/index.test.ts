032e6a2e8f6dd5002fe58d84fd9550249b3d33f9
Chad Stewart <chadrhonanstewart@gmail.com>
2024-08-27 19:22:17 -0500

    Completed refactor of get employee by id controller function.

--- back-end/monolithic-api/test/v1/controllers/employees-controller/index.test.ts
+++ back-end/monolithic-api/test/v1/controllers/employees-controller/index.test.ts
@@ -1,7 +1,10 @@
 import { describe, expect, it, vi } from "vitest";
 import { Request, Response } from "express";
-import { getEmployees } from "../../../../v1/controllers/employees-controller/";
-import { GetEmployeesParams } from "../../../../v1/controllers/employees-controller/util/types/employee-types";
+import { getEmployees, getEmployeeById } from "../../../../v1/controllers/employees-controller/";
+import {
+  GetEmployeeByIdParams,
+  GetEmployeesParams
+} from "../../../../v1/controllers/employees-controller/util/types/employee-types";
 
 describe("Controller: Employees", () => {
   vi.mock("../../../../models/employees", () => {
@@ -29,10 +32,10 @@ describe("Controller: Employees", () => {
   });
 
   vi.mock(
-    "../../../../v1/controllers/employees-controller/util/get-employees-details/handle-get-employee-details-request",
+    "../../../../v1/controllers/employees-controller/util/get-employee-by-id/handle-get-employee-by-id-request",
     () => {
       return {
-        handleGetemployeeDetailsRequest: vi.fn(() => {
+        handleGetEmployeeByIdRequest: vi.fn(() => {
           return {
             statusCode: 200,
             data: "test"
@@ -43,10 +46,10 @@ describe("Controller: Employees", () => {
   );
 
   vi.mock(
-    "../../../../v1/controllers/employees-controller/util/get-employees-details/parse-get-employee-details-request",
+    "../../../../v1/controllers/employees-controller/util/get-employee-by-id/parse-get-employee-by-id-request",
     () => {
       return {
-        parseGetemployeeDetailsRequest: vi.fn(() => "test")
+        parseGetEmployeeByIdRequest: vi.fn(() => "test")
       };
     }
   );
@@ -102,6 +105,64 @@ describe("Controller: Employees", () => {
 
     await getEmployees(
       mockRequest as unknown as Request<GetEmployeesParams>,
+      mockResponse as unknown as Response,
+      mockNextFunc
+    );
+
+    expect(mockNextFunc).toHaveBeenCalled();
+  });
+
+  it("getEmployeeById: Should send a response", async () => {
+    const mockRequest = {
+      params: {
+        employee_id: 1
+      }
+    };
+
+    const mockNextFunc = vi.fn();
+    const mockJsonFunc = vi.fn();
+    const mockStatusFunc = vi.fn(() => {
+      return {
+        json: mockJsonFunc
+      };
+    });
+    const mockResponse = {
+      status: mockStatusFunc
+    };
+
+    await getEmployeeById(
+      mockRequest as unknown as Request<GetEmployeeByIdParams>,
+      mockResponse as unknown as Response,
+      mockNextFunc
+    );
+
+    expect(mockStatusFunc).toHaveBeenCalledWith(200);
+    expect(mockJsonFunc).toHaveBeenCalledWith({
+      data: "test",
+      status: "success"
+    });
+  });
+
+  it("getEmployeeById: Should call the next function", async () => {
+    const mockRequest = {
+      params: {
+        page: 1
+      }
+    };
+
+    const mockNextFunc = vi.fn();
+    const mockJsonFunc = vi.fn();
+    const mockStatusFunc = vi.fn(() => {
+      return {
+        json: mockJsonFunc
+      };
+    });
+    const mockResponse = {
+      status: mockStatusFunc
+    };
+
+    await getEmployeeById(
+      mockRequest as unknown as Request<GetEmployeeByIdParams>,
       mockResponse as unknown as Response,
       mockNextFunc
     );
