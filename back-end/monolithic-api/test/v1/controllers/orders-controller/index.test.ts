a5ab0d74e7637e0348f97383b8a69aa484ea8021
Chad Stewart <chadrhonanstewart@gmail.com>
2024-08-29 18:57:23 -0500

    Completed refactor of get orders controller function.

--- back-end/monolithic-api/test/v1/controllers/orders-controller/index.test.ts
+++ back-end/monolithic-api/test/v1/controllers/orders-controller/index.test.ts
@@ -1,11 +1,12 @@
 import { expect, it, describe, vi, afterEach, beforeEach } from "vitest";
 import { Request, Response } from "express";
 import {
-  getOrderDetails,
-  getOrders,
+  getOrders
+  /* getOrderDetails,
   addOrderAddExistingCustomer,
-  addOrderAddNewCustomer
-} from "../../../../v1/controllers/orders-controller";
+  addOrderAddNewCustomer */
+} from "../../../../v1/controllers/orders-controller/";
+import { GetOrdersParams } from "../../../../v1/controllers/orders-controller/util/types/order-types";
 
 describe("Controller: Orders", () => {
   beforeEach(() => {
@@ -17,13 +18,37 @@ describe("Controller: Orders", () => {
         addOrderExistingCustomer: () => "test"
       };
     });
+
+    vi.mock("../../../../models/orders", () => {
+      return {
+        orders: vi.fn(() => "test"),
+        orderDetails: vi.fn(() => "test")
+      };
+    });
+
+    vi.mock("../../../../v1/controllers/orders-controller/util/get-orders/handle-get-orders-request", () => {
+      return {
+        handleGetOrdersRequest: vi.fn(() => {
+          return {
+            statusCode: 200,
+            data: "test"
+          };
+        })
+      };
+    });
+
+    vi.mock("../../../../v1/controllers/orders-controller/util/get-orders/parse-get-orders-request", () => {
+      return {
+        parseGetOrdersRequest: vi.fn(() => "test")
+      };
+    });
   });
 
   afterEach(() => {
     vi.resetAllMocks();
   });
 
-  it("getOrders: Should send proper data response when called", async () => {
+  it("getOrders: Should send a response", async () => {
     const mockRequest = {
       params: {
         page: 1
@@ -41,12 +66,16 @@ describe("Controller: Orders", () => {
       status: mockStatusFunc
     };
 
-    await getOrders(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
+    await getOrders(
+      mockRequest as unknown as Request<GetOrdersParams>,
+      mockResponse as unknown as Response,
+      mockNextFunc
+    );
 
     expect(mockStatusFunc).toHaveBeenCalledWith(200);
     expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "success",
-      data: "test"
+      data: "test",
+      status: "success"
     });
   });
 
@@ -68,368 +97,12 @@ describe("Controller: Orders", () => {
       status: mockStatusFunc
     };
 
-    await getOrders(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockNextFunc).toHaveBeenCalled();
-  });
-
-  it("getOrders: Should return a response object that says it failed because page isn't a number", async () => {
-    const mockRequest = {
-      params: {
-        page: "hello"
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await getOrders(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(400);
-    expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "failed",
-      error: "orders/'page' must be a number"
-    });
-  });
-
-  it("getOrderDetails: Should send proper data response when called", async () => {
-    const mockRequest = {
-      params: {
-        order_id: 2
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await getOrderDetails(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(200);
-    expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "success",
-      data: "test"
-    });
-  });
-
-  it("getOrderDetails: Should call the next function", async () => {
-    const mockRequest = {
-      params: {
-        order_id: 2
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await getOrderDetails(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockNextFunc).toHaveBeenCalled();
-  });
-
-  it("getOrderDetails: Should return a response object that says it failed because page isn't a number", async () => {
-    const mockRequest = {
-      params: {
-        order_id: "hello"
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await getOrderDetails(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(400);
-    expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "failed",
-      error: "orders/details/'order_id' must be a number"
-    });
-  });
-
-  it("addOrderAddExistingCustomer: Should send proper data response when called", async () => {
-    const mockRequest = {
-      params: {
-        customer_id: "hello"
-      },
-      body: {
-        orders: {
-          type: "object",
-          employee_id: 1,
-          order_date: "2023/12/1",
-          required_date: "2023/12/1",
-          shipped_date: "2023/12/1",
-          ship_via: 1,
-          freight: 1,
-          ship_name: "Hello",
-          ship_address: "Hello",
-          ship_city: "Hello",
-          ship_region: "Hello",
-          ship_postal_code: "Hello",
-          ship_country: "Hello"
-        },
-        order_details: {
-          type: "object",
-          product_id: 1,
-          unit_price: 1,
-          quantity: 1,
-          discount: 1
-        }
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await addOrderAddExistingCustomer(
-      mockRequest as unknown as Request,
-      mockResponse as unknown as Response,
-      mockNextFunc
-    );
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(201);
-    expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "success",
-      data: "test"
-    });
-  });
-
-  it("addOrderAddExistingCustomer: Should call the next function", async () => {
-    const mockRequest = {
-      params: {
-        customer_id: "hello"
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await addOrderAddExistingCustomer(
-      mockRequest as unknown as Request,
+    await getOrders(
+      mockRequest as unknown as Request<GetOrdersParams>,
       mockResponse as unknown as Response,
       mockNextFunc
     );
 
     expect(mockNextFunc).toHaveBeenCalled();
-  });
-
-  it("addOrderAddExistingCustomer: Should return a response object that says it failed because customer_id isn't set", async () => {
-    const mockRequest = {
-      params: {
-        customer_id: ""
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await addOrderAddExistingCustomer(
-      mockRequest as unknown as Request,
-      mockResponse as unknown as Response,
-      mockNextFunc
-    );
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(400);
-    expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "failed",
-      error: "orders/'order_id' must be a number"
-    });
-  });
-
-  it("addOrderAddExistingCustomer: Should return a response object that says it failed because the body data isn't in the proper shape", async () => {
-    const mockRequest = {
-      params: {
-        customer_id: "hello"
-      },
-      body: {
-        test: "test"
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await addOrderAddExistingCustomer(
-      mockRequest as unknown as Request,
-      mockResponse as unknown as Response,
-      mockNextFunc
-    );
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(400);
-    expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "failed",
-      error: "The request body is not what was expected."
-    });
-  });
-
-  it("addOrderAddNewCustomer: Should send proper data response when called", async () => {
-    const mockRequest = {
-      params: {
-        customer_id: "hello"
-      },
-      body: {
-        orders: {
-          type: "object",
-          employee_id: 1,
-          order_date: "2023/12/1",
-          required_date: "2023/12/1",
-          shipped_date: "2023/12/1",
-          ship_via: 1,
-          freight: 1,
-          ship_name: "Hello",
-          ship_address: "Hello",
-          ship_city: "Hello",
-          ship_region: "Hello",
-          ship_postal_code: "Hello",
-          ship_country: "Hello"
-        },
-        order_details: {
-          type: "object",
-          product_id: 1,
-          unit_price: 1,
-          quantity: 1,
-          discount: 1
-        },
-        customers: {
-          company_name: "Hello",
-          contact_name: "Hello",
-          contact_title: "Hello",
-          address: "Hello",
-          city: "Hello",
-          region: "Hello",
-          postal_code: "Hello",
-          country: "Hello",
-          phone: "Hello",
-          fax: "Hello"
-        }
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await addOrderAddNewCustomer(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(201);
-    expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "success",
-      data: "test"
-    });
-  });
-
-  it("addOrderAddNewCustomer: Should call the next function", async () => {
-    const mockRequest = {};
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await addOrderAddNewCustomer(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockNextFunc).toHaveBeenCalled();
-  });
-
-  it("addOrderAddNewCustomer: Should return a response object that says it failed because the body data isn't in the shape that's expected", async () => {
-    const mockRequest = {
-      body: {
-        test: "test"
-      }
-    };
-
-    const mockNextFunc = vi.fn();
-    const mockJsonFunc = vi.fn();
-    const mockStatusFunc = vi.fn(() => {
-      return {
-        json: mockJsonFunc
-      };
-    });
-    const mockResponse = {
-      status: mockStatusFunc
-    };
-
-    await addOrderAddNewCustomer(mockRequest as unknown as Request, mockResponse as unknown as Response, mockNextFunc);
-
-    expect(mockStatusFunc).toHaveBeenCalledWith(400);
-    expect(mockJsonFunc).toHaveBeenCalledWith({
-      status: "failed",
-      error: "The request body is not what was expected."
-    });
   });
 });
