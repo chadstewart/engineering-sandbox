b11b7626bab6b4e88e39876dba6c1a404ae8c563
Chad Stewart <chadrhonanstewart@gmail.com>
2024-08-30 18:46:54 -0500

    Added test suite for get order details controller.

--- back-end/monolithic-api/test/v1/controllers/orders-controller/index.test.ts
+++ back-end/monolithic-api/test/v1/controllers/orders-controller/index.test.ts
@@ -1,51 +1,59 @@
-import { expect, it, describe, vi, afterEach, beforeEach } from "vitest";
+import { expect, it, describe, vi } from "vitest";
 import { Request, Response } from "express";
 import {
-  getOrders
-  /* getOrderDetails,
-  addOrderAddExistingCustomer,
+  getOrders,
+  getOrderDetails
+  /* addOrderAddExistingCustomer,
   addOrderAddNewCustomer */
 } from "../../../../v1/controllers/orders-controller/";
-import { GetOrdersParams } from "../../../../v1/controllers/orders-controller/util/types/order-types";
+import {
+  GetOrderDetailsParams,
+  GetOrdersParams
+} from "../../../../v1/controllers/orders-controller/util/types/order-types";
 
 describe("Controller: Orders", () => {
-  beforeEach(() => {
-    vi.mock("../../../models/orders", () => {
-      return {
-        orders: () => "test",
-        orderDetails: () => "test",
-        addOrderNewCustomer: () => "test",
-        addOrderExistingCustomer: () => "test"
-      };
-    });
+  vi.mock("../../../../models/orders", () => {
+    return {
+      orders: vi.fn(() => "test"),
+      orderDetails: vi.fn(() => "test")
+    };
+  });
 
-    vi.mock("../../../../models/orders", () => {
-      return {
-        orders: vi.fn(() => "test"),
-        orderDetails: vi.fn(() => "test")
-      };
-    });
+  vi.mock("../../../../v1/controllers/orders-controller/util/get-orders/handle-get-orders-request", () => {
+    return {
+      handleGetOrdersRequest: vi.fn(() => {
+        return {
+          statusCode: 200,
+          data: "test"
+        };
+      })
+    };
+  });
 
-    vi.mock("../../../../v1/controllers/orders-controller/util/get-orders/handle-get-orders-request", () => {
+  vi.mock("../../../../v1/controllers/orders-controller/util/get-orders/parse-get-orders-request", () => {
+    return {
+      parseGetOrdersRequest: vi.fn(() => "test")
+    };
+  });
+
+  vi.mock(
+    "../../../../v1/controllers/orders-controller/util/get-order-details/handle-get-order-details-request",
+    () => {
       return {
-        handleGetOrdersRequest: vi.fn(() => {
+        handleGetOrderDetailsRequest: vi.fn(() => {
           return {
             statusCode: 200,
             data: "test"
           };
         })
       };
-    });
+    }
+  );
 
-    vi.mock("../../../../v1/controllers/orders-controller/util/get-orders/parse-get-orders-request", () => {
-      return {
-        parseGetOrdersRequest: vi.fn(() => "test")
-      };
-    });
-  });
-
-  afterEach(() => {
-    vi.resetAllMocks();
+  vi.mock("../../../../v1/controllers/orders-controller/util/get-order-details/parse-get-order-details-request", () => {
+    return {
+      parseGetOrderDetailsRequest: vi.fn(() => "test")
+    };
   });
 
   it("getOrders: Should send a response", async () => {
@@ -99,6 +107,64 @@ describe("Controller: Orders", () => {
 
     await getOrders(
       mockRequest as unknown as Request<GetOrdersParams>,
+      mockResponse as unknown as Response,
+      mockNextFunc
+    );
+
+    expect(mockNextFunc).toHaveBeenCalled();
+  });
+
+  it("getOrderDetails: Should send a response", async () => {
+    const mockRequest = {
+      params: {
+        order_id: 1
+      }
+    };
+
+    const mockNextFunc = vi.fn();
+    const mockJsonFunc = vi.fn();
+    const mockStatusFunc = vi.fn(() => {
+      return {
+        json: mockJsonFunc
+      };
+    });
+    const mockResponse = {
+      status: mockStatusFunc
+    };
+
+    await getOrderDetails(
+      mockRequest as unknown as Request<GetOrderDetailsParams>,
+      mockResponse as unknown as Response,
+      mockNextFunc
+    );
+
+    expect(mockStatusFunc).toHaveBeenCalledWith(200);
+    expect(mockJsonFunc).toHaveBeenCalledWith({
+      data: "test",
+      status: "success"
+    });
+  });
+
+  it("getOrderDetails: Should call the next function", async () => {
+    const mockRequest = {
+      params: {
+        order_id: 1
+      }
+    };
+
+    const mockNextFunc = vi.fn();
+    const mockJsonFunc = vi.fn();
+    const mockStatusFunc = vi.fn(() => {
+      return {
+        json: mockJsonFunc
+      };
+    });
+    const mockResponse = {
+      status: mockStatusFunc
+    };
+
+    await getOrderDetails(
+      mockRequest as unknown as Request<GetOrderDetailsParams>,
       mockResponse as unknown as Response,
       mockNextFunc
     );
