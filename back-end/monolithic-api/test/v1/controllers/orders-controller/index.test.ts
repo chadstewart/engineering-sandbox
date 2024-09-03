de7ed34cdcdc7edc08ca2d08a7a8c776eafc911c
Chad Stewart <chadrhonanstewart@gmail.com>
2024-09-02 20:54:54 -0500

    Refactored test suite for add order add existing customer controller function. Mock for test suite is not working for some reason. Need to figure out why that is and correct it.

--- back-end/monolithic-api/test/v1/controllers/orders-controller/index.test.ts
+++ back-end/monolithic-api/test/v1/controllers/orders-controller/index.test.ts
@@ -2,20 +2,25 @@ import { expect, it, describe, vi } from "vitest";
 import { Request, Response } from "express";
 import {
   getOrders,
-  getOrderDetails
-  /* addOrderAddExistingCustomer,
-  addOrderAddNewCustomer */
+  getOrderDetails,
+  addOrderAddExistingCustomer
+  /* addOrderAddNewCustomer */
 } from "../../../../v1/controllers/orders-controller/";
 import {
+  AddOrderExistingCustomerParams,
+  AddOrderExistingCustomerRequestBody,
   GetOrderDetailsParams,
   GetOrdersParams
 } from "../../../../v1/controllers/orders-controller/util/types/order-types";
+
+type emptyObject = Record<string, never>;
 
 describe("Controller: Orders", () => {
   vi.mock("../../../../models/orders", () => {
     return {
       orders: vi.fn(() => "test"),
-      orderDetails: vi.fn(() => "test")
+      orderDetails: vi.fn(() => "test"),
+      addOrderExistingCustomer: vi.fn(() => "test")
     };
   });
 
@@ -55,6 +60,29 @@ describe("Controller: Orders", () => {
       parseGetOrderDetailsRequest: vi.fn(() => "test")
     };
   });
+
+  vi.mock(
+    "../../../../v1/controllers/orders-controller/util/add-order-existing-customer/handle-add-order-existing-customer-request",
+    () => {
+      return {
+        handleAddOrderExistingCustomerRequest: vi.fn(() => {
+          return {
+            statusCode: 200,
+            data: "test"
+          };
+        })
+      };
+    }
+  );
+
+  vi.mock(
+    "../../../../v1/controllers/orders-controller/util/add-order-existing-customer/parse-add-order-existing-customer-request",
+    () => {
+      return {
+        parseAddOrderExistingCustomerRequest: vi.fn(() => "test")
+      };
+    }
+  );
 
   it("getOrders: Should send a response", async () => {
     const mockRequest = {
@@ -165,6 +193,96 @@ describe("Controller: Orders", () => {
 
     await getOrderDetails(
       mockRequest as unknown as Request<GetOrderDetailsParams>,
+      mockResponse as unknown as Response,
+      mockNextFunc
+    );
+
+    expect(mockNextFunc).toHaveBeenCalled();
+  });
+
+  it("addOrderAddExistingCustomer: Should send a response", async () => {
+    const mockRequest = {
+      params: {
+        customer_id: 1
+      },
+      body: {
+        orders: {
+          type: "object",
+          employee_id: 1,
+          order_date: "2023/12/1",
+          required_date: "2023/12/1",
+          shipped_date: "2023/12/1",
+          ship_via: 1,
+          freight: 1,
+          ship_name: "Hello",
+          ship_address: "Hello",
+          ship_city: "Hello",
+          ship_region: "Hello",
+          ship_postal_code: "Hello",
+          ship_country: "Hello"
+        },
+        order_details: {
+          type: "object",
+          product_id: 1,
+          unit_price: 1,
+          quantity: 1,
+          discount: 1
+        }
+      }
+    };
+
+    const mockNextFunc = vi.fn();
+    const mockJsonFunc = vi.fn();
+    const mockStatusFunc = vi.fn(() => {
+      return {
+        json: mockJsonFunc
+      };
+    });
+    const mockResponse = {
+      status: mockStatusFunc
+    };
+
+    await addOrderAddExistingCustomer(
+      mockRequest as unknown as Request<
+        AddOrderExistingCustomerParams,
+        emptyObject,
+        AddOrderExistingCustomerRequestBody
+      >,
+      mockResponse as unknown as Response,
+      mockNextFunc
+    );
+
+    expect(mockStatusFunc).toHaveBeenCalledWith(201);
+    expect(mockJsonFunc).toHaveBeenCalledWith({
+      data: "test",
+      status: "success"
+    });
+  });
+
+  it("AddOrderAddExistingCustomer: Should call the next function", async () => {
+    const mockRequest = {
+      params: {
+        customer_id: 1
+      }
+    };
+
+    const mockNextFunc = vi.fn();
+    const mockJsonFunc = vi.fn();
+    const mockStatusFunc = vi.fn(() => {
+      return {
+        json: mockJsonFunc
+      };
+    });
+    const mockResponse = {
+      status: mockStatusFunc
+    };
+
+    await addOrderAddExistingCustomer(
+      mockRequest as unknown as Request<
+        AddOrderExistingCustomerParams,
+        emptyObject,
+        AddOrderExistingCustomerRequestBody
+      >,
       mockResponse as unknown as Response,
       mockNextFunc
     );
