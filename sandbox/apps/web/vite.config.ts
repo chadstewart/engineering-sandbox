fd067002659b73c9e6ad84c6aad4d0f2f243f295
Chad Stewart <chadrhonanstewart@gmail.com>
2026-06-12 17:19:31 -0500

    Installed and started initial set up for Tanstack Router.

--- sandbox/apps/web/vite.config.ts
+++ sandbox/apps/web/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react()],
 })
