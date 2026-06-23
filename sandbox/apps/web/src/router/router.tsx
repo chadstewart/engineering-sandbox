b254ec70a995e4569e07c1018e06162cb9e38d2e
Chad Stewart <chadrhonanstewart@gmail.com>
2026-06-23 08:26:42 -0500

    Added tailwind to react project.

--- sandbox/apps/web/src/router/router.tsx
+++ sandbox/apps/web/src/router/router.tsx
@@ -20,6 +20,9 @@ const rootRoute = createRootRoute({
         <Link to="/about" className="[&.active]:font-bold">
           About
         </Link>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </div>
       <hr />
       <App />
