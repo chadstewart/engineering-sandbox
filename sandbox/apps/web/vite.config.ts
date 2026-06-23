84fdff4967e1f325c1f9425e30a3435c2ea32122
Chad Stewart <chadrhonanstewart@gmail.com>
2026-06-23 08:26:42 -0500

    Added tailwind to react project.

--- sandbox/apps/web/vite.config.ts
+++ sandbox/apps/web/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
@@ -10,5 +11,7 @@ export default defineConfig({
     //   target: 'react',
     //   autoCodeSplitting: true,
     // }),
-    react()],
+    react(),
+    tailwindcss()
+  ],
 })
