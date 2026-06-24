7d246068a0fa0cfb4c04953fe2ff3d23d38bd779
Chad Stewart <chadrhonanstewart@gmail.com>
2026-06-24 08:28:37 -0500

    Continuing migration of original engineering sandbox layout by migratingsidebar nav.

---
+++ sandbox/apps/web/src/lib/types/nav-item-props.ts
@@ -0,0 +1,25 @@
+import { type LinkOptions } from "@tanstack/react-router";
+// import { IconType } from "react-icons";
+
+export type NavItemProps = {
+  text: string;
+  // icon: IconType;
+  children?: NavItemChildren[];
+  url?: LinkOptions["to"];
+  params?: object;
+  iconAlt: string;
+  activeLink?: boolean;
+  externalLinkAlt?: string;
+  isProtectedLink?: boolean;
+};
+
+type NavItemChildren = {
+  text: string;
+  // icon: IconType;
+  url: LinkOptions["to"];
+  params?: object;
+  iconAlt: string;
+  activeLink?: boolean;
+  externalLinkAlt?: string;
+  isProtectedLink?: boolean;
+};
