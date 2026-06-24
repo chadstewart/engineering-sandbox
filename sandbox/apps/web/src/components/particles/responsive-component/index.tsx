1f63009ee309974794de1dfe67b515affa1e84d7
Chad Stewart <chadrhonanstewart@gmail.com>
2026-06-24 08:10:42 -0500

    Started migration of original engineering-sandbox layout to new front-end project.

---
+++ sandbox/apps/web/src/components/particles/responsive-component/index.tsx
@@ -0,0 +1,14 @@
+import React, { type ReactNode } from "react";
+import { useScreenSize } from "@/lib/hooks/use-screen-size";
+
+interface ResponsiveComponentProps {
+  children: (props: { size: number }) => ReactNode;
+}
+
+const ResponsiveComponent: React.FC<ResponsiveComponentProps> = ({ children }) => {
+  const size = useScreenSize();
+
+  return <>{children({ size })}</>;
+};
+
+export default ResponsiveComponent;
