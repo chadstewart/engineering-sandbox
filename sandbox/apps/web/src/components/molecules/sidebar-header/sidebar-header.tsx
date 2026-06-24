06c808b7e7bac33d3bebe8fb1168ad1317e40337
Chad Stewart <chadrhonanstewart@gmail.com>
2026-06-24 08:44:21 -0500

    Completed most of the sidebar migration. Just realized this project used shad-cn and I'm going to need to import that. This is fine because I honestly planned to use it but I had forgotten.

--- sandbox/apps/web/src/components/molecules/sidebar-header/sidebar-header.tsx
+++ sandbox/apps/web/src/components/molecules/sidebar-header/sidebar-header.tsx
@@ -1,4 +1,4 @@
-import { Logo } from "../logo/logo";
+import { Logo } from "@/components/molecules/logo/logo";
 
 export const SidebarHeader = () => {
   return (
--- sandbox/apps/web/src/components/organisms/sidebar/sidebar.tsx
+++ sandbox/apps/web/src/components/organisms/sidebar/sidebar.tsx
@@ -1,30 +1,11 @@
 import { SidebarHeader } from "@/components/molecules/sidebar-header/sidebar-header";
 import { SidebarContentWrapper } from "@/components/molecules/sidebar-content/sidebar-content-wrapper";
-import { type NavItemProps } from "@/lib/types/nav-item-props";
-import { SidebarContent } from "@/components/molecules/sidebar-content/sidebar-content";
 
-interface SidebarProps {
-  storybook?: {
-    displayLoginContent: {
-      handleLogin: () => Promise<void>;
-      handleLogout: () => Promise<void>;
-      handleSignUp: () => Promise<void>;
-      isAuthenticated: boolean;
-      userName: string | undefined;
-    };
-    navItems: NavItemProps[];
-  };
-}
-
-const Sidebar = ({ storybook }: SidebarProps) => {
+const Sidebar = () => {
   return (
     <aside className="flex flex-col h-screen sticky top-0 p-3 w-96 border-r-2">
       <SidebarHeader />
-      {storybook ? (
-        <SidebarContent navItems={storybook.navItems} storybook={storybook.displayLoginContent} />
-      ) : (
-        <SidebarContentWrapper />
-      )}
+      <SidebarContentWrapper />
     </aside>
   );
 };
