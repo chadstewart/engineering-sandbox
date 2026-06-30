2e7f993e8e3b78392691116c2ceec7e994eee1f4
Chad Stewart <chadrhonanstewart@gmail.com>
2026-06-30 09:17:37 -0500

    Fixing some build issues with properly building the app.

--- sandbox/apps/web/vite.config.ts
+++ sandbox/apps/web/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,4 +14,9 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  }
 })
--- sandbox/packages/ui/src/components/ui/sheet.tsx
+++ sandbox/packages/ui/src/components/ui/sheet.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
 import { Dialog as SheetPrimitive } from "radix-ui"
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "../../lib/utils"
+import { Button } from "./button"
 import { XIcon } from "lucide-react"
 
 function Sheet({ ...props }: React.ComponentProps<typeof SheetPrimitive.Root>) {
