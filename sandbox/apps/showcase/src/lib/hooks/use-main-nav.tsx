00bea8394caaff3e617cb378d634026df448d12e
Chad Stewart <chadrhonanstewart@gmail.com>
2026-07-08 09:24:34 -0500

    Updating nav bar with new nav items.

--- sandbox/apps/showcase/src/lib/hooks/use-main-nav.tsx
+++ sandbox/apps/showcase/src/lib/hooks/use-main-nav.tsx
@@ -1,76 +1,28 @@
 import { type NavItemProps } from "../types/nav-item-props";
-
-/* import { IoHomeSharp } from "react-icons/io5";
-import { FaCat } from "react-icons/fa";
+import { IoHomeSharp } from "react-icons/io5";
 import { IoIosInformationCircle } from "react-icons/io";
-import { BiSolidNetworkChart } from "react-icons/bi";
-import { IoCart } from "react-icons/io5";
-import { TbPackages } from "react-icons/tb";
-import { PiUsersFill } from "react-icons/pi";
-import { BiDetail } from "react-icons/bi";
-import { IoMdPersonAdd } from "react-icons/io"; */
+import { RiGalleryView } from "react-icons/ri";
 
 const useMainNav = () => {
 	const navList: NavItemProps[] = [
-		/* {
+		{
       url: "/",
       text: "Home",
       icon: IoHomeSharp,
       iconAlt: "Home page link icon"
     },
     {
-      url: "/cute-animals",
-      text: "Cute Animals",
-      icon: FaCat,
-      iconAlt: "Cat page link icon"
-    },
-    {
-      text: "Orders",
-      icon: IoCart,
-      iconAlt: "Order dropdown link icon",
-      url: "/orders/details/$page",
-      params: { page: 1 }
-    },
-    {
-      url: "/products/details/$page",
-      params: { page: 1 },
-      text: "Products",
-      icon: TbPackages,
-      iconAlt: "Product page link icon"
-    },
-    {
-      text: "Customers",
-      icon: PiUsersFill,
-      iconAlt: "Customer page link icon",
-      children: [
-        {
-          url: "/customers/details/$page",
-          params: { page: 1 },
-          text: "Details",
-          icon: BiDetail,
-          iconAlt: "Details page link icon"
-        },
-        {
-          url: "/customers/add-customer",
-          text: "Add Customer",
-          icon: IoMdPersonAdd,
-          iconAlt: "Add Customer link icon",
-          isProtectedLink: true
-        }
-      ]
-    },
-    {
-      url: "/about",
+      url: "/",
       text: "About",
       icon: IoIosInformationCircle,
       iconAlt: "About page link icon"
     },
     {
-      url: "/architecture",
-      text: "Architecture",
-      icon: BiSolidNetworkChart,
-      iconAlt: "Architecture page link icon"
-    }*/
+      url: "/",
+      text: "Showcase",
+      icon: RiGalleryView,
+      iconAlt: "Showcase page link icon"
+    }
 	];
 
 	return navList;
