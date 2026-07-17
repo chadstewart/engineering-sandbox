75d7bd55ee61147c8f029aaa235860e910520add
Chad Stewart <chadrhonanstewart@gmail.com>
2026-07-17 10:14:32 -0500

    Updated nav to add ability to use external lists as the previous set up didn't allow to add any links other than internal links.

--- sandbox/apps/showcase/src/lib/types/nav-item-props.ts
+++ sandbox/apps/showcase/src/lib/types/nav-item-props.ts
@@ -2,21 +2,38 @@ import type { LinkOptions } from "@tanstack/react-router";
 import type { IconType } from "react-icons";
 
 export interface NavItemProps extends NavItemBase {
+	appUrl: NavItemUrlBase;
 	children?: NavItemChildren[];
 }
 
 interface NavItemChildren extends NavItemBase {
-	url: LinkOptions["to"];
+	appUrl: NavItemUrlBase;
 }
 
-type NavItemBase = {
+export type NavItemBase = {
 	id?: number;
 	text: string;
 	icon: IconType;
-	url?: LinkOptions["to"];
 	params?: object;
 	iconAlt: string;
 	activeLink?: boolean;
-	externalLinkAlt?: string;
 	isProtectedLink?: boolean;
 };
+
+type NavItemUrlBase = OnlyExternalUrls | OnlyInternalUrls;
+
+interface OnlyInternalUrls {
+	internalUrl: NavItemInternalUrl;
+	externalUrl?: never;
+	externalLinkAlt?: never;
+}
+
+interface OnlyExternalUrls {
+	internalUrl?: never;
+	externalUrl: NavItemExternalUrl;
+	externalLinkAlt: string;
+}
+
+type NavItemExternalUrl = string;
+
+type NavItemInternalUrl = LinkOptions["to"];
