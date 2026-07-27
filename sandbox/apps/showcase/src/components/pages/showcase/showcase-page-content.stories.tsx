ec8ee6d112d48543666d529af6837b0bc5c9bd06
Chad Stewart <chadrhonanstewart@gmail.com>
2026-07-27 09:18:11 -0500

    Started building out the showcase page.

--- sandbox/apps/showcase/src/components/pages/showcase/showcase-page-content.stories.tsx
+++ sandbox/apps/showcase/src/components/pages/showcase/showcase-page-content.stories.tsx
@@ -1,9 +1,13 @@
+import { handleProjectData } from "../../../services/handle-project-data";
 import { ShowcasePageContent } from "./showcase-page-content";
+
 
 const storyConfig = {
   title: "Showcase Project/Pages/Showcase"
 };
 
+const projectData = handleProjectData();
+
 export default storyConfig;
 
-export const ShowcaseStory = () => <ShowcasePageContent />;
+export const ShowcaseStory = () => <ShowcasePageContent projectData={projectData} />;
