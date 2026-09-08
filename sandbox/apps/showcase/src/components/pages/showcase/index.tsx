ecab5cba242fc543674dc9174f8a49a187864d9d
Chad Stewart <chadrhonanstewart@gmail.com>
2026-09-08 08:00:38 -0500

    Doing some work around storybook stories not rendering properly because they aren't build built (incomplete).

--- sandbox/apps/showcase/src/components/pages/showcase/index.tsx
+++ sandbox/apps/showcase/src/components/pages/showcase/index.tsx
@@ -1,8 +1,10 @@
 import { ShowcasePageContent } from "@/components/pages/showcase/showcase-page-content";
 import { updateTitle } from "@/lib/util/update-title";
+import { handleProjectData } from "@/services/handle-project-data";
 
 export const ShowcasePage = () => {
 	updateTitle("Showcase");
+	const data = handleProjectData();
 
-	return <ShowcasePageContent />;
+	return <ShowcasePageContent projectData={data} />;
 };
